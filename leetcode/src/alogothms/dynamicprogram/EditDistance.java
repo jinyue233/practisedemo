c642736fbfe85fd52adc4a8b4168ece131b1afa6
jinyue <23472568+jinyue233@users.noreply.github.com>
2018-11-23 17:30:01 +0800

    1，上传编辑距离的带备忘录的递归解法。

--- leetcode/src/alogothms/dynamicprogram/EditDistance.java
+++ leetcode/src/alogothms/dynamicprogram/EditDistance.java
@@ -34,16 +34,22 @@ public class EditDistance {
         }
     }
 
+    /**
+     * 带备忘录的暴力搜索法。
+     * @param word1
+     * @param word2
+     * @return
+     */
     public static int minDistance2(String word1, String word2) {
         int w1 = word1.length();
         int w2 = word2.length();
-        int[][] meno = new int[w1 + 1][w2 + 1];
+        int[][] meno = new int[w1][w2];
         for (int i = 0; i < w1; i++) {
             for (int j = 0; j < w2; j++) {
                 meno[i][j] = -1;
             }
         }
-        return minDistanceMeno(word1, word2, w1, w2, meno);
+        return minDistanceMeno(word1, word2, w1 - 1, w2 - 1, meno);
     }
 
     public static int minDistanceMeno(String word1, String word2, int w1, int w2, int[][] meno) {
@@ -64,6 +70,7 @@ public class EditDistance {
         }
 
         if (meno[w1][w2] >= 0) {
+            //System.out.println(meno[w1][w2]);
             return meno[w1][w2];
         }
         if (word1.charAt(w1) == word2.charAt(w2)) {
@@ -77,8 +84,8 @@ public class EditDistance {
         }
     }
     public static void main(String[] args) {
-        String word1 = "intention";
-        String word2 = "execution";
+        String word1 = "horse";
+        String word2 = "ros";
         System.out.println(minDistance2(word1, word2));
 
     }
