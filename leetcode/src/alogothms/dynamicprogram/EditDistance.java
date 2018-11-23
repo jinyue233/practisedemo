6f8d70c038c6b053aaf7452e91c56ae816aee076
jinyue <23472568+jinyue233@users.noreply.github.com>
2018-11-23 22:02:58 +0800

    1，上传编辑距离的自底向上的解法。

--- leetcode/src/alogothms/dynamicprogram/EditDistance.java
+++ leetcode/src/alogothms/dynamicprogram/EditDistance.java
@@ -83,10 +83,41 @@ public class EditDistance {
             return meno[w1][w2];
         }
     }
+
+    /**
+     * 自底向上求解。
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public static int minDistance3(String word1, String word2) {
+        int w1 = word1.length();
+        int w2 = word2.length();
+        int[][] result = new int[w1 + 1][w2 + 1];
+        for (int i = 0; i <= w1; i++) {
+            result[i][0] = i;
+        }
+
+        for (int i = 0; i <= w2; i++) {
+            result[0][i] = i;
+        }
+
+        for (int i = 1; i <= w1; i++) {
+            for (int j = 1; j <= w2; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    result[i][j] = result[i - 1][j - 1];
+                } else {
+                    result[i][j] = Math.min(result[i - 1][j - 1], Math.min(result[i][j - 1], result[i - 1][j])) + 1;
+                }
+            }
+        }
+        return result[w1][w2];
+    }
+
     public static void main(String[] args) {
         String word1 = "horse";
         String word2 = "ros";
-        System.out.println(minDistance2(word1, word2));
+        System.out.println(minDistance3(word1, word2));
 
     }
 
